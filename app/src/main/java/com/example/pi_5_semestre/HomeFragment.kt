cb819fb9c6acee050aa9a2e56914c02e665bf74f
Felippe Pacomio <felipepacomio@gmail.com>
2024-04-22 10:19:05 -0300

    Image Slide Show in Home Fragment

--- app/src/main/java/com/example/pi_5_semestre/HomeFragment.kt
+++ app/src/main/java/com/example/pi_5_semestre/HomeFragment.kt
@@ -5,17 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -30,11 +28,26 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.ferias, "Confira nossa programação de férias!"))
+        imageList.add(SlideModel(R.drawable.facaparte, "Tem vontade de fazer parte da equipe? Junte-se a nós!"))
+        imageList.add(SlideModel(R.drawable.psocial, "Confira nossos próximos projetos sociais na comunidade do Jardim dos Eucaliptos"))
+        imageList.add(SlideModel(R.drawable.cestabasica, "Entrega de Cestas Básicas Maio 2024"))
+        imageList.add(SlideModel(R.drawable.newsimage3, "A equipe está maior! Conheça nossos novos patrocínios!"))
+        //imageList.add(SlideModel("https://bit.ly/3fLJf72", "And people do that."))
+
+        val imageSlider = rootView.findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP) // for all images
+
+        return rootView
     }
 
     companion object {
